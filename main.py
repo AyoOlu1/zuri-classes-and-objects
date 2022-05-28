b2a5d45cf8541ae9d93b0e9c4bede37ad4d4d351
AyoOlu1 <oluwayemivictor15@gmail.com>
2022-05-28 13:58:24 +0100

    first commit

--- main.py
+++ main.py
@@ -6,13 +6,38 @@ class Student:
         self.tracks = tracks
         self.scores = score
         
+    def change_name(self, new_name):
+        self.name = new_name
+        if type(new_name) == str:
+            return new_name
+        else:
+            return "Enter a string!"
+
+    def change_age(self, new_age):
+        self.age = new_age
+        if type(new_age) == int:
+            return new_age
+        else:
+            return "Enter a number!"
+
+    def add_track(self, new_track):
+        track_list = self.tracks
+        track_list.append(str(new_track))
+        return track_list
+
+    def get_score(self):
+        return self.scores
+
 
 
 
 Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
 
+
 # Expected methods
-Bob.change_name("Peter")
-Bob.change_age(34)
+Bob.name
+Bob.change_name("jsgdjshd")
 Bob.add_track("UI/UX")
+Bob.change_age(30)
+Bob.add_track("sdsd")
 Bob.get_score()
